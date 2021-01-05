f02b79d96f7e44e0f656039eb71e54448aef17e7
VShka <viktor.shkarupa@yandex.ru>
2021-01-05 18:49:34 +0300

    feat(KS-04): Добавил общий header

---
+++ src/components/header/header.vue
@@ -0,0 +1,28 @@
+<template>
+  <header class="header">
+    <router-link to="/" class="header__logo">
+      Это HEADER
+    </router-link>
+  </header>
+</template>
+<style lang="scss" scoped>
+.header {
+  background: #fff;
+  box-shadow: 0 0 10px #a3a3a3;
+  padding: 22px 24px;
+  border-bottom: 1px solid #e2e2ea;
+  position: fixed;
+  width: 100%;
+  left: 0;
+  top: 0;
+  z-index: 10;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+@media screen and (max-width: 1023px) {
+  .header {
+    padding: 10px 15px;
+  }
+}
+</style>
