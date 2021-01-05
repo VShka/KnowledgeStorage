75eeb3b77a6af18e49017e9ec98121bb2282a8b8
VShka <viktor.shkarupa@yandex.ru>
2021-01-05 14:37:22 +0300

    Новая вьюха страница 404 + прописал роуты для нее

---
+++ src/views/page-404/page-404.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="page-404">
+    <h1 class="page-404__title">404</h1>
+    <p class="page-404__subtitle">
+      Страница не существует или находится в разработке.
+    </p>
+  </section>
+</template>
+<script lang="ts">
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.page-404 {
+  width: 80%;
+  margin: 0 auto;
+}
+</style>
