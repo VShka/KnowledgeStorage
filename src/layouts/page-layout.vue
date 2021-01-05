1f42a0b8a944bf7b2a35aa08047687670777bd05
VShka <viktor.shkarupa@yandex.ru>
2021-01-05 17:04:27 +0300

    feat(KS-01): сделал общую верстку для приложения и прописал роут

---
+++ src/layouts/page-layout.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app" class="page-layout">
+    <main class="main">
+      <router-view />
+    </main>
+  </div>
+</template>
+<style lang="scss" scoped>
+.main {
+  padding-left: 56px;
+  padding-top: 73px;
+  transition: 0.2s ease-in-out;
+}
+@media screen and (max-width: 1023px) {
+  .main {
+    padding: 60px 15px 0;
+  }
+}
+</style>
