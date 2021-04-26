fdd14c7e61bdade6cc566039d2ada901ac50880c
VShka <shkarupa.viktor@gmail.com>
2021-04-26 23:17:30 +0300

    Пофиксил сайдбар меню

--- src/components/nav-accordeon/nav-accordeon.vue
+++ src/components/nav-accordeon/nav-accordeon.vue
@@ -13,34 +13,6 @@
         </router-link>
       </div>
     </div>
-    <!-- <div class="nav-accordeon__container">
-      <div class="nav-accordeon__icon_preview">&#xf1cb;</div>
-      <div class="nav-accordeon__icon_small">&#xf1cb;</div>
-      <div class="nav-accordeon__icon_large">
-        <router-link to="/knowledge">
-          <h3 class="nav-accordeon__link-title">Программирование</h3>
-          <p class="nav-accordeon__link-text">'Hello, world!'</p>
-        </router-link>
-      </div>
-    </div>
-    <div class="nav-accordeon__container">
-      <div class="nav-accordeon__icon_preview">&#xf17d;</div>
-      <div class="nav-accordeon__icon_small">&#xf17d;</div>
-      <div class="nav-accordeon__icon_large">
-        <h3 class="nav-accordeon__link-title">Математика</h3>
-        <p class="nav-accordeon__link-text">a + b = b + a</p>
-      </div>
-    </div>
-    <div class="nav-accordeon__container">
-      <div class="nav-accordeon__icon_preview">&#xf26e;</div>
-      <div class="nav-accordeon__icon_small">&#xf26e;</div>
-      <div class="nav-accordeon__icon_large">
-        <h3 class="nav-accordeon__link-title">Философия</h3>
-        <p class="nav-accordeon__link-text">
-          Пунктуальность - воровка времени.
-        </p>
-      </div>
-    </div> -->
   </nav>
 </template>
 
--- src/components/sidebar/sidebar.vue
+++ src/components/sidebar/sidebar.vue
@@ -9,23 +9,10 @@
       enter-to-class="sidebar__menu-link-text_fade-enter-from"
       leave-to-class="sidebar__menu-link-text_fade-leave-to"
     >
-      <div v-show="isCollapsed" class="sidebar__menu-link-text">
+      <div class="sidebar__menu-link-text">
         <AccordeonMenu />
       </div>
     </transition>
-    <!-- <nav class="sidebar__menu">
-      <ul class="sidebar__menu-list">
-        <li class="sidebar__menu-item">
-          <router-link
-            class="sidebar__menu-link"
-            :class="{ 'sidebar__menu-link_collapsed': isCollapsed }"
-            active-class="sidebar__menu-link_active"
-            :to="{ name: 'index' }"
-          >
-          </router-link>
-        </li>
-      </ul>
-    </nav> -->
     <button
       class="sidebar__collapse-btn"
       :class="{ 'sidebar__collapse-btn_collapsed': isCollapsed }"
